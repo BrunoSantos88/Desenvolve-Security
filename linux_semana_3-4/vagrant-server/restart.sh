c6fa007607eb9393ef099da7b5af98027e2ee9c7
BrunoSantos88 <brunosantos88>
2024-03-15 21:01:39 -0300

    atualizando

--- linux_semana_3-4/vagrant-server/restart.sh
+++
@@ -1,7 +0,0 @@
-if [ $resposta_http -ne 200 ]
-then
-mail -s "Problema no servidor" brunosantosc1@gmailcom<<del
-Houve um problema no servidor e os usuarios pararam de ter acesso ao conteúdo web.
-del
-    systemctl restart apache2
-fi
