1804f33d3f5e653005a8d23716df7ebcd8c40acf
BrunoSantos88 <brunosantos88>
2024-04-05 13:29:13 -0300

    desafio

--- desafio_desenvolve_1/index.js
+++ desafio_desenvolve_1/index.js
@@ -1,65 +1,64 @@
+// Seletor html /// index.hmtl //
+const $startGameButton = document.querySelector(".start-quiz");
+const $nextQuestionButton = document.querySelector(".next-question");
+const $questionsContainer = document.querySelector(".questions-container");
+const $questionText = document.querySelector(".question");
+const $answersContainer = document.querySelector(".answers-container");
+const $answers = document.querySelectorAll(".answer");
 
-//Seletor html botoes//
-const $startGameButton = document.querySelector(".start-quiz") 
-const $nextQuestionButton = document.querySelector(".next-question")
-const $questionsContainer = document.querySelector(".questions-container") 
-const $questionText = document.querySelector(".question") 
-const $answersContainer = document.querySelector(".answers-container")
-const $answers = document.querySelectorAll(".answer")
+// Contagem de perguntas /// iniciar apartir -> start quiz //
+let currentQuestionIndex = 0;
+let totalCorrect = 0;
 
+// Clicar start quiz ///
+$startGameButton.addEventListener("click", startGame);
+$nextQuestionButton.addEventListener("click", displayNextQuestion);
 
-
-//Contagen de perguntas
-let currentQuestionIndex = 1
-let totalCorrect = 1
-
-// Clicar star quiz.
-$startGameButton.addEventListener("click", startGame)
-$nextQuestionButton.addEventListener("click", displayNextQuestion)
-
-//Funçoes start quiz esconder///
+// Funções start quiz esconder
 function startGame() {
-  $startGameButton.classList.add("hide")
-  $questionsContainer.classList.remove("hide")
-  displayNextQuestion()
+  $startGameButton.classList.add("hide");
+  $questionsContainer.classList.remove("hide");
+  displayNextQuestion();
 }
+
 function displayNextQuestion() {
-  resetState()
+  resetState();
   if (questions.length === currentQuestionIndex) {
-    return finishGame()
+    return finishGame();
   }
-  $questionText.textContent = questions[currentQuestionIndex].question
+  $questionText.textContent = questions[currentQuestionIndex].question;
   questions[currentQuestionIndex].answers.forEach(answer => {
-    const newAsnwer = document.createElement("button")
-    newAsnwer.classList.add("button", "answer")
-    newAsnwer.textContent = answer.text
+    const newAnswer = document.createElement("button");
+    newAnswer.classList.add("button", "answer");
+    newAnswer.textContent = answer.text;
     if (answer.correct) {
-      newAsnwer.dataset.correct = answer.correct
+      newAnswer.dataset.correct = answer.correct;
     }
-    $answersContainer.appendChild(newAsnwer)
-    newAsnwer.addEventListener("click", selectAnswer)
-  })
+    $answersContainer.appendChild(newAnswer);
+    newAnswer.addEventListener("click", selectAnswer);
+  });
 }
+
 function resetState() {
-  while($answersContainer.firstChild) {
-    $answersContainer.removeChild($answersContainer.firstChild)
+  while ($answersContainer.firstChild) {
+    $answersContainer.removeChild($answersContainer.firstChild);
   }
 
-  document.body.removeAttribute("class")
-  $nextQuestionButton.classList.add("hide")
+  document.body.removeAttribute("class");
+  $nextQuestionButton.classList.add("hide");
 }
 
-//funçoes correta e incorreta
+// Funções correta e incorreta //
 function selectAnswer(event) {
   const answerClicked = event.target;
   document.querySelectorAll(".answer").forEach(button => {
     button.disabled = true;
   });
   if (answerClicked.dataset.correct) {
-    answerClicked.classList.add("correct");
-    totalCorrect++; // 
+    answerClicked.classList.add("correct"); ///green//
+    totalCorrect++;
   } else {
-    answerClicked.classList.add("incorrect");
+    answerClicked.classList.add("incorrect"); ///red/
   }
   document.querySelectorAll(".answer").forEach(button => {
     if (button.dataset.correct) {
@@ -68,102 +67,60 @@ function selectAnswer(event) {
   });
   $nextQuestionButton.classList.remove("hide");
   currentQuestionIndex++;
-} 
+}
 
-function displayNextQuestion() {
-  resetState();
-  
-  if (questions.length === currentQuestionIndex) {
-    return finishGame();
+// Função para finalizar e feedback quantas acertou ///
+function finishGame() {
+  const totalQuestions = questions.length;
+  const performance = Math.floor((totalCorrect / totalQuestions) * 100);
+
+  let message = "";
+
+  switch (true) {
+    case performance >= 90:
+      message = "Excelente :)";
+      break;
+    case performance >= 70:
+      message = "Muito bom :)";
+      break;
+    case performance >= 50:
+      message = "Bom";
+      break;
+    default:
+      message = "Pode melhorar :(";
+      break;
   }
 
-  const currentQuestion = questions[currentQuestionIndex];
-  $questionText.textContent = currentQuestion.question;
-
-  currentQuestion.answers.forEach(answer => {
-    const newAnswer = document.createElement("button");
-    newAnswer.classList.add("button", "answer");
-    newAnswer.textContent = answer.text;
-    if (answer.correct) {
-      newAnswer.dataset.correct = answer.correct;
-    }
-    $answersContainer.appendChild(newAnswer);
-
-    newAnswer.addEventListener("click", selectAnswer);
-  });
-
+  ///Criar botao refazer QUIZ/// Após fim de jogo ///
+  $questionsContainer.innerHTML = `
+      <p class="final-message">
+          Você acertou ${totalCorrect} de ${totalQuestions} questões!
+          <span>Resultado: ${message}</span>
+      </p>
+      <button onclick=window.location.reload() class="button">
+          Refazer Quiz!  
+      </button>
+  `;
 }
 
-//Função de proxima pergunta//
-function displayNextQuestion() {
-  resetState();
-  
-  if (questions.length === currentQuestionIndex) {
-    return finishGame();
-  }
-
-  const currentQuestion = questions[currentQuestionIndex];
-  $questionText.textContent = currentQuestion.question;
-  currentQuestion.answers.forEach(answer => {
-    const newAnswer = document.createElement("button");
-    newAnswer.classList.add("button", "answer");
-    newAnswer.textContent = answer.text;
-    if (answer.correct) {
-      newAnswer.dataset.correct = answer.correct;
-    }
-    $answersContainer.appendChild(newAnswer);
-
-    newAnswer.addEventListener("click", selectAnswer);
-  });
-}
-
-// Apos terminar, enconder start quiz (escoder)//
-function endGame() {
-  document.querySelector('.final-message').classList.remove('hide'); 
-  document.querySelector('.controls-container').classList.add('hide');
-}
-document.querySelector('.start-quiz').addEventListener('click', function() {
-  startTimer(); 
-});
-function displayNextQuestion() {
-  resetState();
-  
-  if (questions.length === currentQuestionIndex) {
-    return finishGame();
-  }
-  const currentQuestion = questions[currentQuestionIndex];
-  $questionText.textContent = currentQuestion.question;
-  currentQuestion.answers.forEach(answer => {
-    const newAnswer = document.createElement("button");
-    newAnswer.classList.add("button", "answer");
-    newAnswer.textContent = answer.text;
-    if (answer.correct) {
-      newAnswer.dataset.correct = answer.correct;
-    }
-    $answersContainer.appendChild(newAnswer);
-
-    newAnswer.addEventListener("click", selectAnswer);
-  });
-}
-
-//Quiz Banco de perguntas//
+///banco de perguntas//
 const questions = [
   {
-    question: "Qual data que começou Desenvolve?",
+    question: "Qual foi a data de início do Desenvolve?",
     answers: [
-      { text: "Dia 29 de janeiro", correct: true, feedback: "Correto! Esse dia foi a primeira live de integração do curso Desenvolve!" },
-      { text: "Dia 02 de Fevereiro", correct: false, feedback: "Incorreto! Essa data não ocorreu nada." },
-      { text: "12 de Dezembro", correct: false, feedback: "Incorreto! Essa data não ocorreu nada." },
-      { text: "29 de Fevereiro", correct: false, feedback: "Incorreto! Esse dia liberou a plataforma Alura." }
+      { text: "29 de janeiro", correct: true, feedback: "Correto! Esse dia foi a primeira live de integração do curso Desenvolve!" },
+      { text: "02 de fevereiro", correct: false, feedback: "Incorreto! Essa data não teve eventos relacionados ao Desenvolve." },
+      { text: "12 de dezembro", correct: false, feedback: "Incorreto! Essa data não teve eventos relacionados ao Desenvolve." },
+      { text: "29 de fevereiro", correct: false, feedback: "Incorreto! Esse dia não existe no calendário." }
     ]
   },
   {
-    question: "Que dia foi o primeiro encontro?",
+    question: "Qual foi o dia do primeiro encontro?",
     answers: [
-      { text: "Dia 7, em uma quarta-feira!", correct: false, feedback: "Incorreto!" },
-      { text: "Dia 8 em uma quinta-feira!", correct: true, feedback: "Correto! Esse dia foi o primeiro encontro à noite, às 20h!" },
-      { text: "Dia 4  em uma terça-feira!", correct: false, feedback: "Incorreto!!" },
-      { text: "Dia 02 no mesmo dia que começou!", correct: false, feedback: "Incorreto!" }
+      { text: "02 e 07 de fevereiro, em uma quarta e quinta-feira.", correct: false, feedback: "Incorreto!" },
+      { text: "05 e 08 de fevereiro, em uma terça e quinta-feira.", correct: true, feedback: "Correto! Esse foi o primeiro encontro à noite, às 20h!" },
+      { text: "04 de janeiro, em uma segunda-feira, em dois horários diferentes.", correct: false, feedback: "Incorreto!" },
+      { text: "06 de fevereiro, em um sábado.", correct: false, feedback: "Incorreto!" }
     ]
   },
   {
@@ -176,7 +133,7 @@ const questions = [
     ]
   },
   {
-    question: 'O desenvolve oferece curso pela plataforma Alura?',
+    question: 'O Desenvolve oferece cursos pela plataforma Alura?',
     answers: [
       { text: "Verdadeiro", correct: true, feedback: "Correto! Os cursos são oferecidos pela plataforma Alura." },
       { text: "Falso", correct: false, feedback: "Incorreto! Os cursos são oferecidos pela plataforma Alura." },
@@ -185,47 +142,21 @@ const questions = [
   {
     question: 'Qual foi o dia da aula inaugural do Desenvolve?',
     answers: [
-      { text: 'Dia 02 de janeiro às 20h', correct: false, feedback: "Incorreto! Não houve nada neste dia. A primeira live foi no dia 29 de janeiro." },
-      { text: 'Dia 29 de janeiro às 19h', correct: true, feedback: "Correto! Esse dia foi a primeira live do grupo Desenvolve." },
-      { text: 'Dia 02 de Fevereiro às 20h', correct: false, feedback: "Incorreto! A primeira live foi no dia 29 de janeiro." },
-      { text: 'Dia 08 de Fevereiro às 8h da manhã', correct: false, feedback: "Incorreto! A primeira live foi no dia 29 de janeiro." },
+      { text: '02 de janeiro às 20h', correct: false, feedback: "Incorreto! Não houve nenhum evento nesta data. A primeira live foi no dia 29 de janeiro." },
+      { text: '29 de janeiro às 19h', correct: true, feedback: "Correto! Essa foi a primeira live do grupo Desenvolve." },
+      { text: '02 de fevereiro às 20h', correct: false, feedback: "Incorreto! A primeira live foi no dia 29 de janeiro." },
+      { text: '08 de fevereiro às 8h da manhã', correct: false, feedback: "Incorreto! A primeira live foi no dia 29 de janeiro." },
+    ]
+  },
+  
+  {
+    question: 'Qual é a data para entregar o primeiro projeto?',
+    answers: [
+      { text: '16 de abril no grupo WhatsApp do Desenvolve.', correct: false },
+      { text: '9 de maio às 19h pelo Telegram.', correct: false  },
+      { text: '17 de abril no email dos mentores.', correct: true },
+      { text: 'Entregar na próxima reunião para os mentores.', correct: false },
     ]
   },
 ];
-
-
-// Função para finalizar e feedback quantas acertou///
-function finishGame() {
-  const totalQuestions = questions.length;
-  const performance = Math.floor((totalCorrect / totalQuestions) * 100);
-
-  let message = "";
-
-  switch (true) {
-      case performance >= 90:
-          message = "Excelente :)";
-          break;
-      case performance >= 70:
-          message = "Muito bom :)";
-          break;
-      case performance >= 50:
-          message = "Bom";
-          break;
-      default:
-          message = "Pode melhorar :(";
-          break;
-  }
-
-  ///Criar botao centralizado apos finalizar quiz /// 
-  $questionsContainer.innerHTML = `
-      <p class="final-message">
-          Você acertou ${totalCorrect} de ${totalQuestions} questões!
-          <span>Resultado: ${message}</span>
-      </p>
-      <button onclick=window.location.reload() class="button">
-          Refazer Quiz!  
-      </button>
-  `;
-}
-
 
