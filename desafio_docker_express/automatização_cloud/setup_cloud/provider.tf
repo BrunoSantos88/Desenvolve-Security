1cd95633367b7c2f715aed98987dca1b7f0c6fa6
BrunoSantos88 <brunosantos88>
2024-05-07 19:47:56 -0300

    desafio2

--- desafio_docker_express/automatização_cloud/setup_cloud/provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-  version = "~> 3.0"
-}
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "expresss_key_bucket_desenvolve"
-  acl    = "private"
-}
---
+++ desafio_docker_express/cloud_network/s3.tf
@@ -0,0 +1,4 @@
+resource "aws_s3_bucket" "express_key" {
+  bucket = "expresss_key_bucket_desenvolve"
+  acl    = "private"
+}
