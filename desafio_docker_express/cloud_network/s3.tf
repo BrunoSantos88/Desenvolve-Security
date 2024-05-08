d8439e8b6f3a564d155273da8cf02375417b4f45
Bruno Santos <91704169+BrunoSantos88@users.noreply.github.com>
2024-05-07 21:48:56 -0300

    Update s3.tf

--- desafio_docker_express/cloud_network/s3.tf
+++ desafio_docker_express/cloud_network/s3.tf
@@ -1,8 +1,3 @@
 resource "aws_s3_bucket" "express_key" {
   bucket = "express-bucket-desafio2"
 }
-
-resource "aws_s3_bucket_acl" "express_key_acl" {
-  bucket = aws_s3_bucket.express_key.bucket
-  acl    = "private"
-}
