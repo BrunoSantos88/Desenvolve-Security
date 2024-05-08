e4889c5db0ec076703d43f54850246f13816086a
Bruno Santos <91704169+BrunoSantos88@users.noreply.github.com>
2024-05-07 22:09:05 -0300

    Create var.tf

---
+++ desafio_docker_express/var.tf
@@ -0,0 +1,9 @@
+variable "express_availability_zone" {
+  description = "Zona de disponibilidade"
+  default     = "us-east-1a" 
+}
+
+variable "express_region" {
+  description = "Região AWS"
+  default     = "us-east-1"
+}
