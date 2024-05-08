a087d40ebda1900d6a54c4ae34ff65448507bd9c
Bruno Santos <91704169+BrunoSantos88@users.noreply.github.com>
2024-05-07 22:05:33 -0300

    Update network.tf

--- desafio_docker_express/cloud_network/network.tf
+++ desafio_docker_express/cloud_network/network.tf
@@ -1,7 +1,8 @@
 resource "aws_vpc" "app_express" {
     cidr_block           = "192.168.0.0/16"
     enable_dns_support   = true            
-    enable_dns_hostnames = true           
+    enable_dns_hostnames = true     
+    availability_zone        = var.region_express_a
     
     tags = {
         Name        = "desenvolve"
