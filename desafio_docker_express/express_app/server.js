3f8e3fd30b2b81003f4fe5ea30d5b4403791bb2a
Bruno Santos <91704169+BrunoSantos88@users.noreply.github.com>
2024-05-12 23:44:31 +0000

    docker_express

--- desafio_docker_express/express_app/server.js
+++ desafio_docker_express/express_app/server.js
@@ -1,20 +1,10 @@
 const express = require("express");
-const { collectDefaultMetrics, register } = require('prom-client');
 
 const app = express();
 
-collectDefaultMetrics();
 
 app.get("/", (req, res) => res.send("Hello World!!"));
 
-app.get('/metrics', async (_req, res) => {
-  try {
-    res.set('Content-Type', register.contentType);
-    res.end(await register.metrics());
-  } catch (err) {
-    res.status(500).end(err);
-  }
-});
 
 const PORT = 3000;
 
