d5b51efe30fe91ef4d91a5058b464ba676b82dc3
Bruno Santos <91704169+BrunoSantos88@users.noreply.github.com>
2024-05-12 16:32:31 +0000

    express_docker

--- desafio_docker_express/express_app/server.js
+++ desafio_docker_express/express_app/server.js
@@ -1,9 +1,12 @@
-const express = require("express");
 
-const app = express();
+const http = require("http")
+const express = require("express")
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+const app = express()
 
-app.listen(3000);
+app.get("/", (req, res) => res.send("Hello World!!"))
+
+const server = http.createServer(app)
+
+// FAZ O SERVIDOR RESPONDER NA PORTA 3000
+server.listen(3000, () => console.log('Servidor rodando na porta %s.', 3000))
