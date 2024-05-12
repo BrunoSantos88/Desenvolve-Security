a8e4be4584d74bc9fa8acb681b879f350ad6cb62
Bruno Santos <91704169+BrunoSantos88@users.noreply.github.com>
2024-05-12 04:36:35 +0000

    dockercompose

--- desafio_docker_express/express_app/server.js
+++ desafio_docker_express/express_app/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const { collectDefaultMetrics, register } = require('prom-client');
+
+collectDefaultMetrics();
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor escutando na porta ${PORT}`);
+app.get('/metrics', async (_req, res) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  } catch (err) {
+    res.status(500).end(err.toString());
+  }
+});
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server is running on port 3000');
 });
