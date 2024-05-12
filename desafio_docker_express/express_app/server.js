8e16eb4c4cc02f9eae9f7552f2b15eb498e306f3
Bruno Santos <91704169+BrunoSantos88@users.noreply.github.com>
2024-05-12 06:12:05 +0000

    express_metrics

--- desafio_docker_express/express_app/server.js
+++ desafio_docker_express/express_app/server.js
@@ -1,24 +1,9 @@
-//const
 const express = require('express');
-const { register, collectDefaultMetrics } = require('prom-client');
-
-// Initialize Prometheus metrics collection
-collectDefaultMetrics();
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
-
-// Endpoint for exposing Prometheus metrics
-app.get('/metrics', (req, res) => {
-  try {
-    res.set('Content-Type', register.contentType);
-    res.end(register.metrics());
-  } catch (err) {
-    res.status(500).send(`Error fetching metrics: ${err.message}`);
-  }
 });
 
 const PORT = process.env.PORT || 3000;
