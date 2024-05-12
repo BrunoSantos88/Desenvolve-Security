b0e1b95218192d0a3fd052a82aab2d3b8afcf5f4
Bruno Santos <91704169+BrunoSantos88@users.noreply.github.com>
2024-05-12 07:29:54 +0000

    express_app

--- desafio_docker_express/express_app/server.js
+++ desafio_docker_express/express_app/server.js
@@ -1,25 +1,11 @@
 const express = require('express');
-const { register, collectDefaultMetrics } = require('prom-client');
-
 const app = express();
 
-collectDefaultMetrics();
-
-
-app.get('/metrics', async (_req, res) => {
-  try {
-    res.set('Content-Type', register.contentType);
-    res.end(await register.metrics());
-  } catch (err) {
-    res.status(500).send(`Erro ao obter métricas: ${err.message}`);
-  }
-});
-
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, '0.0.0.0', () => {
+
+app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
