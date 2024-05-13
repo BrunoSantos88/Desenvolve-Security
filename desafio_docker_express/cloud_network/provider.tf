bfeec166cd22eb5086c0267444f58ed898a0cc49
BrunoSantos88 <brunosantos88>
2024-05-13 12:44:11 -0300

    action

--- desafio_docker_express/cloud_network/provider.tf
+++ desafio_docker_express/cloud_network/provider.tf
@@ -1,8 +1,4 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+provider "aws" {
+  region = var.express_zona_a
+  version = "1.0`"
 }
