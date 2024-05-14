d64e10d56acf10cdaacfb8659fc454704531e255
Bruno Santos <91704169+BrunoSantos88@users.noreply.github.com>
2024-05-14 04:03:16 +0000

    desafio2

--- desafio_docker_express/cloud_network/instance.tf
+++ desafio_docker_express/cloud_network/instance.tf
@@ -5,10 +5,6 @@ resource "aws_instance" "express_server" {
   disable_api_termination     = true
   subnet_id                   = aws_subnet.express_network_a.id
   associate_public_ip_address = true                        
-  
-
-  #### SCRIPT APACHE
-  user_data = file("express_dependencia.sh")
 
   vpc_security_group_ids = [
      aws_security_group.express-firewall.id
