872594e375c26d1c420b905ae9fbf1698ab33b94
Bruno Santos <91704169+BrunoSantos88@users.noreply.github.com>
2024-05-14 05:04:09 +0000

    projeto2

--- desafio_docker_express/cloud_network/instance.tf
+++ desafio_docker_express/cloud_network/instance.tf
@@ -2,7 +2,6 @@ resource "aws_instance" "express_server" {
   ami                         = var.express_ami
   instance_type               = "t2.micro"            
   availability_zone           = var.express_zona_a 
-  disable_api_termination     = true
   subnet_id                   = aws_subnet.express_network_a.id
   associate_public_ip_address = true    
 
@@ -11,10 +10,7 @@ resource "aws_instance" "express_server" {
   vpc_security_group_ids = [
      aws_security_group.express-firewall.id
   ]
-  root_block_device {
-    delete_on_termination = true
-    volume_size           = 10
-    volume_type           = "gp2"
+
 
   }
   tags = {
@@ -22,5 +18,4 @@ resource "aws_instance" "express_server" {
     Environment = "BrunoSantos"
     OS          = "Ubuntu20:04 "
     Managed     = "Desenvolve"
-  }
 }
