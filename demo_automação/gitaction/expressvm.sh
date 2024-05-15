9ccc1d55e103106ac4996dee6c304232765b0df7
BrunoSantos88 <brunosantos88>
2024-05-15 16:11:25 -0300

    runner

--- demo_automação/gitaction/expressvm.sh
+++ demo_automação/gitaction/expressvm.sh
@@ -7,3 +7,26 @@
   sudo npm install
   sudo npm install express
   sudo docker-compose up -d
+
+
+    #bin/bash
+  sudo apt update -y
+  sudo apt install docker.io -y
+  sudo adduser $USER docker
+  # Install Git && commandos
+  #sudo apt-get update -y
+ # sudo apt-get install git -y
+ # sudo git clone https://github.com/BrunoSantos88/Desenvolve-Trilha-SI.git
+  sudo cd Desenvolve-Trilha-SI/ && git checkout devtest2 && cd express_app
+  # install nodejs && commmandos
+  #sudo apt-get update -y
+  #sudo apt install curl -y
+ # sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+  #sudo source ~/.bashrc
+  #sudo nvm install --lts
+  #sudo nvm alias default 24.1.2
+  sudo node -v npm -v
+  sudo npm init -y
+  sudo npm install
+  sudo npm install express
+  sudo docker-compose up -d
--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
   sudo apt update -y
   sudo apt install docker.io -y
   sudo adduser $USER docker
-  sudo cd Desenvolve-Trilha-SI/ && git checkout devtest2 && cd express_app
   sudo npm init -y
   sudo npm install
   sudo npm install express
