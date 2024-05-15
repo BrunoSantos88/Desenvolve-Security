1eb663c2c9759e3efec19d146101ee89ab2a64c2
BrunoSantos88 <brunosantos88>
2024-05-15 16:32:16 -0300

    runner

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
   #bin/bash
-  sudo apt update -y
-  sudo apt install docker.io -y
-  sudo adduser $USER docker
-  sudo docker-compose up -d
+ cd express_app
+ sudo docker-compose up -d 
+ curl http://localhost:3000
