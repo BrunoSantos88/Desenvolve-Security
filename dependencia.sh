50b130eb25c7809878e970361304039f21b34432
Bruno Santos <91704169+BrunoSantos88@users.noreply.github.com>
2024-05-17 11:43:21 -0300

    Update and rename install.sh to dependencia.sh

---
+++ dependencia.sh
@@ -0,0 +1,9 @@
+#bin/bash
+sudo apt update -y
+sudo apt  install docker.io -y
+sudo apt  install docker-compose -y
+sudo apt update -y
+sudo apt install nodejs -y
+sudo apt install npm -y
+sudo apt-get install git -y
+echo "install complete"
